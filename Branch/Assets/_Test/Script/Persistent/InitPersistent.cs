5d175e89cdf8b1661f059157469a778adc1bf164
KRJaeh0 <145985933+JJH0204@users.noreply.github.com>
2026-01-22 01:09:08 +0900

    Revert "External Paid Asset Package Unity Package (managed as a free-fit directory) Add auto-call capability to Unity Package, clean up asset directory"

---
+++ Branch/Assets/_Test/Script/Persistent/InitPersistent.cs
@@ -0,0 +1,67 @@
+using Managers;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EManagerType
+{
+    GameManager,
+    GUIManager,
+    DataManager,
+    DungeonManager,
+    DungeonStateManager,
+    MonsterManager,
+    PoolManager,
+    SceneManager,
+    SoundManager,
+    EventManager,
+    MapManager,
+}
+
+[Serializable]
+public struct ManagerInfo
+{
+    public EManagerType managerType;
+    public GameObject managerPrefab;
+}
+
+public class InitPersistent : MonoBehaviour
+{
+    [SerializeField] private ManagerInfo[] managersPrefabs;
+
+    private void Awake()
+    {
+        try
+        {
+            Debug.Log($"persistent InitPersistent Awake()");
+            
+            Dictionary<EManagerType, GameObject> createdManagers = new ();
+            
+            foreach (ManagerInfo managerInfo in managersPrefabs)
+            {
+                if (!managerInfo.managerPrefab) continue;
+                if (createdManagers.ContainsKey(managerInfo.managerType)) continue;
+
+                Debug.Log($"{managerInfo.managerPrefab.name} Instantiate()");
+                GameObject managerInstance = Instantiate(managerInfo.managerPrefab);
+                
+                createdManagers.Add(managerInfo.managerType, managerInstance);
+            }
+            
+            Debug.Log($"persistent LoadPersistent()"); // 모든 매니저가 로드되었음을 GameManager에 알림
+            // GameManager.Instance.MainCamera = createdManagers[EManagerType.MainCamera];
+            GameManager.Instance.SceneLoaded();
+            
+            Destroy(gameObject);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+    
+    private void OnDestroy()
+    {
+        GameManager.Instance.EnterTitle();
+    }
+}
